23c3b22f983c400a43798c4f600827e2b379bdc8
Hresa <gresapetro379@gmail.com>
2021-04-23 15:44:18 +0300

    add: Project fails

---
+++ Bilder_Lviv/#src/assets/scripts/script.ts
@@ -0,0 +1,7 @@
+/*_Function  support browser webp css_*/
+
+console.log('work')
+
+console.log("it's i ")
+
+let a: string = "kakasa"; 
---
+++ Bilder_Lviv/dist/assets/scripts/script.js
@@ -0,0 +1,5 @@
+"use strict";
+/*_Function  support browser webp css_*/
+console.log('work');
+console.log("it's i ");
+let a = "kakasa";
