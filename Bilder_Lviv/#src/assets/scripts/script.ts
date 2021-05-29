9ff085c4f603b4c17bc3cbd6a1ca6156eba13d5d
Hresa <gresapetro379@gmail.com>
2021-05-29 11:40:28 +0300

    Removed folder from repository

--- Bilder_Lviv/#src/assets/scripts/script.ts
+++
@@ -1,7 +0,0 @@
-/*_Function  support browser webp css_*/
-
-console.log('work')
-
-console.log("it's i ")
-
-let a: string = "kakasa"; 
--- Bilder_Lviv/dist/assets/scripts/script.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-/*_Function  support browser webp css_*/
-console.log('work');
-console.log("it's i ");
-let a = "kakasa";
